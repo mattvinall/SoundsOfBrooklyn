e0ac61b0b677748d93cfc7d79c211cc6fc04a4d3
Matt Vinall <mattvinall@Matts-MacBook-Pro.local>
2018-11-15 23:01:10 -0500

    first commit  - set up HTML and CSS and added all partials and sound files for soundboard

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+//Document ready
+$(function({
+    //create object that holds all character arrays
+    const audio = {
+        //create array of Jake quotes (mp3)
+        jake : [
+            'sounds/jake-breakfastBurrito.mp3',
+            
+        ]
+         
+    }
+};
+
+console.log(audio);
