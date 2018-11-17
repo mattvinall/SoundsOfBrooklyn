1e55d3ae1228e3d8bb1ae68e35f4fb127c196359
Matt Vinall <mattvinall@Matts-MacBook-Pro.local>
2018-11-16 21:36:18 -0500

    added a new sounds folder and added a function to my app that the sound would play and pause on click

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,104 @@
-//Document ready
-$(function({
     //create object that holds all character arrays
-    const audio = {
+    // const characterQuotes  = {
         //create array of Jake quotes (mp3)
-        jake : [
-            'sounds/jake-breakfastBurrito.mp3',
-            
-        ]
-         
-    }
-};
+        const soundBite = {
+            jake : [
+                'sounds/Jake-breakfastBurrito.mp3',
+                'sounds/Jake-backstreetBoys.mp3',
+                'sounds/Jake-littleSpoon.mp3',
+                'sounds/Jake-guitar.mp3',
+                'sounds/Jake-coolCoolCool.mp3'
+            ],
+             // array for terry quotes
+             terry : [
+                'sounds/Terry-Yogurt.mp3',
+                'sounds/Terry-brains.mp3',
+                'sounds/Terry-ebonyFalcon.mp3',
+                'sounds/Terry-farts.mp3',
+                'sounds/Terry-lovesLavendar.mp3'
+            ],
+             // array for holdt quotes
+            holdt : [
+                'sounds/Holdt-deathThreat.mp3',
+                'sounds/Holdt-adrenaline.mp3',
+                'sounds/Holdt-breasts.mp3',
+                'sounds/Holdt-favJoke.mp3',
+                'sounds/Holdt-thirsty.mp3',
+                'sounds/Holdt-twitter.mp3'
+            ],
+             // array for rosa quotes
+             rosa : [
+                'sounds/Rosa-badass.mp3',
+                'sounds/Rosa-funny.mp3',
+                'sounds/Rosa-garbage.mp3',
+                'sounds/Rosa-sorry.mp3',
+                'sounds/Rosa-wheresMyFile.mp3'
+            ],
+             // array for Boyle quotes
+             boyle : [
+                'sounds/Boyle-checkOutThisKaboose.mp3',
+                'sounds/Boyle-BM.mp3',
+                'sounds/Boyle-getMyLadyOff.mp3',
+                'sounds/Boyle-STDs.mp3',
+                'sounds/Boyle-turkeyStock.mp3'
+            ],
+             // array for Gina quotes
+             gina : [
+                'sounds/Gina-bloodlust.mp3',
+                'sounds/Gina-100emoji.mp3',
+                'sounds/Gina-highHorse.mp3',
+                'sounds/Gina-notToBrag.mp3',
+                'sounds/Gina-spaceheater'
+            ],
+            // array for Amy quotes
+            amy : [
+                'sounds/Amy-dogs.mp3',
+                'sounds/Amy-faily.mp3',
+                'sounds/Amy-themThangs.mp3',
+                'sounds/Amy-drink5.mp3',
+                'sounds/Amy-waddupBro.mp3'
+            ],
+            skully : [
+                'sounds/Skully-food.mp3'
+            ],
+            hitchcock : [
+                'sounds/Hitchcock-getWokeSkully.mp3'
+            ]
+        }
 
-console.log(audio);
+
+        //set variable outside of scope so that we can avoid scoping issue
+        let isplaying = false;
+
+        function audioPlay(name) {
+            let player = document.getElementById('quote');
+            let randomQuote = soundBite[name][Math.floor(Math.random() * soundBite[name].length)];
+
+            if (!isplaying) {
+                player.src = randomQuote;
+                player.play();  
+                // is playing
+                isplaying = true;    
+            } else {
+                player.pause();
+                // is paused
+                isplaying = false;
+            }
+        }
+
+
+
+
+
+    // Create function that will play a random quote from the character array 
+    
+    // When user clicks on character - play the randomized quote
+    
+    // switch the SRC attribute dynamically?
+
+    // When user clicks another character, stop playing that clip and play the other 
+
+    //
+
+
+
